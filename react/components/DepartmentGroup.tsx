d88ea1cbce95d02b19c8b12314bd47cf6c1cba8f
Christian Ramirez <imchristiandev@gmail.com>
2022-01-19 22:36:44 -0500

    finished custom search bar

---
+++ react/components/DepartmentGroup.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+type Props = {
+  departments: [Category],
+  handleSetSlug: any
+}
+
+type Category = {
+  id: number,
+  name: string,
+  slug: string
+}
+
+const DepartmentGroup = ({ departments, handleSetSlug }: Props) => {
+  console.log("Mi grupo de departamentos es:", departments);
+
+  const onHandleSetSlug = (event: any) => {
+    handleSetSlug(`${event.target.value}/$\{term\}&map=ft`)
+  }
+
+  const departmentOptions: any = departments.map((department: Category) => {
+    return (
+      <option
+        value={department.slug}
+        key={department.id}
+      >
+        {department.name}
+      </option>
+    )
+  })
+  return (
+    <select
+      onChange={onHandleSetSlug}
+      defaultValue="value0"
+    >
+      <option disabled value="value0">Seleccione una opción</option>
+      {departmentOptions}
+    </select>
+  )
+}
+
+export default DepartmentGroup
